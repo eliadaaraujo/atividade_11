7edd15f90fc30153e6738a7caccde54c9e3d1ab3
eli <eliadadearaujolinhares@gmail.com>
2025-10-01 14:31:33 -0300

    Add files via upload

---
+++ atividade11.py
@@ -0,0 +1,4 @@
+# Crie uma lista com 5 nomes de colegas da turma e exiba o primeiro e o último nome.
+colegas = ['Marcelo', 'Otávio', 'Levi', 'Jonas', 'Calebe']
+print("Primeiro nome:", colegas[0])
+print("Último nome:", colegas[4])
